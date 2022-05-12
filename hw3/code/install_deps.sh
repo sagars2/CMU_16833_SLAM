dd957458cf7e6e5247930567577dccf2d47bf7c5
Sagar Sachdev <sagar22sachdev@gmail.com>
2022-05-12 17:01:04 -0400

    hw4

--- hw3/code/install_deps.sh
+++
@@ -1,6 +0,0 @@
-# Necessary: pip install -r requirements will build then install, so if numpy is not existing the procedure will fail.
-pip install numpy
-pip install cffi
-pip install scipy
-pip install matplotlib
-pip install git+https://github.com/theNded/PySPQR.git
